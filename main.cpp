b77d45dc10378e71bead37473d5cfd2087e46c7f
unknown <ykshv96@gmail.com>
2025-09-10 07:07:07 +0300

    initial commit: inputOutputUtf8RutextConsole

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>                 // ввод/вывод
+using namespace std;
+
+int main() {
+    system("chcp 65001 > nul");    // переключаем кодовую страницу консоли в UTF-8
+
+    int age;                      // переменная для возраста
+    cout << "введите ваш возраст.. ";
+    cin >> age;
+    cout << "вам " << age << " лет" << endl;
+
+    system("pause");               // пауза, чтобы окно не закрывалось сразу
+    return 0;
+}
