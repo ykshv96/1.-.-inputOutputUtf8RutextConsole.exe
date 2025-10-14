cd19b0c71f5eae1151fc61a37f5d3c52cc8f6570
unknown <ykshv96@gmail.com>
2025-10-14 06:28:07 +0300

    Имя папок?

---
+++ 3 сalculater/main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    system("chcp 65001 > nul");
+
+    double one = 0.0;
+    double two = 0.0;
+
+    string operation = "+";
+
+    cout << "Введите первое число: ";
+    cin >> one;
+
+    cout << "Введите второе число: ";
+    cin >> two;
+
+    cout << "Введите операцию (help, +, -, *, /): ";
+    cin >> operation;
+
+    if (operation == "help") {
+        cout << "тут подробное описание. введите два компонента и выберите действие которое вы хотите сделать с ними" << endl;
+        return 0;
+    }
+
+    double result = 0.0;
+
+    if (operation == "+") {
+        result = one + two;
+    } else if (operation == "-") {
+        result = one - two;
+    } else if (operation == "*") {
+        result = one * two;
+    } else if (operation == "/") {
+        if (two == 0) {
+            cout << "внимание. ноль не приемлемое число." << endl;
+            return 1;
+        }
+        result = one / two;
+    } else {
+        cout << "неизвестная операция: " << operation << endl;
+        return 1;
+    }
+
+    cout << "на жри свой ответ: " << result << endl;
+    system("pause");
+
+    return 0;
+}
